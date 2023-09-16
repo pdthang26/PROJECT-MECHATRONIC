c9276eb94a8471fcaa07daeb780350c80cb1199a
Trần Phạm Trung Hy <125619209+TranPhamTrungHy@users.noreply.github.com>
2023-09-16 18:22:23 +0700

    GUI ver 5

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -7,25 +7,32 @@ from PIL import ImageTk, Image
 import serial
 from math import *
 
+'''Tạo các biến cần thiết cho chương trình'''
+
 # Tạo mảng bao gồm các thành phần trên giao diện
 objects_0 = []
 objects_1 = []
 objects_2 = []
+objects_3 = []
 
 # Bit kết thúc
 stop_bit = '\n'
 
+# Các biến dùng truyền UART
+b_uart=f_uart=p_uart =angle_uart=gps_uart= None
+
 ''' Chức năng giao diện '''
 
-uart = None
 # Hàm cho nút connect
 def connect_uart():
     # kích hoạt nút Manual, Auto
     for obj in objects_2:
         obj['state'] = 'normal'
 
-    global uart
+    global b_uart,f_uart,p_uart,angle_uart,gps_uart
+
     selected_port = com_port.get()
+    selected_gps = gps_port.get()
     selected_rate = int(rate_port.get())
     selected_stop = stop_port.get()
     select_data = int(data_port.get())
@@ -50,13 +57,71 @@ def connect_uart():
         return output
     data_bit_value = switch_case_2(select_data)
 
-    uart = serial.Serial(
+    # Tạo kết nối UART với các biến bánh trước, bánh sau, phanh
+    b_uart = f_uart = p_uart= angle_uart = serial.Serial(
         port=selected_port,
         baudrate=selected_rate,
         stopbits=stop_bit_value,
         bytesize=data_bit_value,
         timeout=1  # Timeout cho phép đọc từ giao diện UART
     )
+
+    # # Tạo kết nối UART với biến GPS
+    # gps_uart = serial.Serial(
+    #     port = selected_gps,
+    #     baudrate=9600,
+    #     stopbits= stop_bit_value,
+    #     bytesize= data_bit_value,
+    #     timeout= 1
+    # )
+
+    # def read_gps_data():
+    #     # Đọc dữ liệu GPS từ UART
+    #     gps_data = gps_uart.readline().decode().strip()
+    #     # Xử lý dữ liệu từ UART
+    #     if gps_data.startswith('$GPRMC'):
+    #         data = gps_data.split(',')
+
+    #         # Kiểm tra xem dữ liệu có đủ trường thông tin và trạng thái là "Active" hay không
+    #         if len(data) >= 7 and data[2] == 'A':
+    #             latitude = float(data[3])  # Vĩ độ
+    #             longitude = float(data[5])  # Kinh độ
+
+    #             # Chuyển đổi đơn vị vĩ độ và kinh độ từ độ, phút sang độ thập phân
+    #             lat_degrees = math.floor(latitude / 100)
+    #             lat_minutes = latitude - (lat_degrees * 100)
+    #             latitude_decimal = lat_degrees + (lat_minutes / 60)
+
+    #             long_degrees = math.floor(longitude / 100)
+    #             long_minutes = longitude - (long_degrees * 100)
+    #             longitude_decimal = long_degrees + (long_minutes / 60)
+
+    #             # In ra giá trị vĩ độ và kinh độ
+    #             long_display['text'] = longitude_decimal
+    #             lat_display['text'] = latitude_decimal
+
+    #             # Lập lịch cho việc đọc dữ liệu tiếp theo sau 200ms
+    #             root.after(200, read_uart_data)
+
+    # # Lập lịch cho việc đọc dữ liệu UART sau 200ms
+    # root.after(200, read_uart_data)
+   
+    while True:
+        #Đọc dữ liệu từ UART 
+        motion = angle_uart.readline().decode().strip()
+
+        # Xử lý dữ liệu từ UART
+        if 'A' in motion and 'V' in motion:
+            # Tách chuỗi thành các phần riêng biệt dựa trên ký tự xuống dòng
+            angle_data, vel_data = motion.split('\n')
+                
+            # Xử lý dữ liệu góc
+            if angle_data.startswith('A'):
+                angle_display['text'] = angle_data[1:]
+                
+            # Xử lý dữ liệu vận tốc
+            if vel_data.startswith('V'):
+                vel_display['text'] = vel_data[1:]
 
     # tạo popup thông báo kết nối thành công 
     popup = tk.Toplevel()
@@ -72,7 +137,6 @@ def connect_uart():
     y = root.winfo_rooty() + (root_height - popup_height) // 2
     popup.geometry(f"{popup_width}x{popup_height}+{x}+{y}")
      
-    
     canvas = tk.Canvas(popup, width=100, height=100,bd=0)
     canvas.place(x=1,y=1)
 
@@ -91,13 +155,11 @@ def connect_uart():
 
     popup.mainloop()
    
-
 # Hàm cho nút Open
 def open_click():
     for obj in objects_0:
         obj['state'] = 'normal'
-    long_display['text'] = 'hello'
-    lat_display['text'] = 'hi'
+    
 
 # Hàm cho nút Close
 def close_click():
@@ -119,9 +181,13 @@ root.resizable(height=False, width=False)
 com_label = tk.Label(root, text="COM Port:", bg='#A8DF8E')
 com_label.place(x=180, y=5)
 
+#Tạo nhãn cho ô chọn cổng COM GPS
+gps_label = tk.Label(root, text='GPS Port:',bg='#A8DF8E')
+gps_label.place(x=300,y= 5)
+
 # Tạo nhãn cho ô chọn Baudrate
 rate_label = tk.Label(root, text='Baud Rate', bg='#A8DF8E')
-rate_label.place(x=300, y=5)
+rate_label.place(x=420, y=5)
 
 # Tạo nhãn cho Data Bits
 data_label = tk.Label(root,text='Data Bit:',bg='#A8DF8E')
@@ -145,21 +211,37 @@ brake_label.place(x= 205, y= 265 )
 
 # Tạo nhãn cho Longitude
 long_label = tk.Label(root, text='Longitude', bg='#A8DF8E')
-long_label.place(x=550, y=5)
+long_label.place(x=800, y=5)
 
 # Tạo ô hiển thị cho Longitude
 long_display = tk.Label(root, relief=tk.SUNKEN,anchor=tk.W, padx=10,bg='white')
-long_display.place(x=550, y=30, height=30, width=230)
+long_display.place(x=800, y=30, height=30, width=230)
 
-# Tạo nhãn cho Lattitude
-lat_label = tk.Label(root, text='Lattitude', bg='#A8DF8E')
-lat_label.place(x=800, y=5)
+# Tạo nhãn cho Latitude
+lat_label = tk.Label(root, text='Latitude', bg='#A8DF8E')
+lat_label.place(x=800, y=65)
 
-# Tạo ô hiển thị cho Lattitude
+# Tạo ô hiển thị cho Latitude
 lat_display = tk.Label(root, relief=tk.SUNKEN,anchor=tk.W,padx=10,bg='white')
-lat_display.place(x=800, y=30, height=30, width=230)
+lat_display.place(x=800, y=90, height=30, width=230)
 
-# Lấy danh sách tất cả các cổng COM
+# Tạo nhãn cho hiển thị Angle
+angle_label = tk.Label(root,text='Angle',bg='#A8DF8E')
+angle_label.place(x=550,y= 5)
+
+#Tạo ô hiển thị cho Angle
+angle_display = tk.Label(root,relief=tk.SUNKEN,anchor=tk.W,padx=10,bg='white')
+angle_display.place(x=550,y=30,height=30,width=100)
+
+# Tạo nhãn cho Velocity
+vel_label = tk.Label(root,text='Velocity',bg='#A8DF8E')
+vel_label.place(x= 670,y=5 )
+
+#Tạo ô hiển thị Velocity
+vel_display= tk.Label(root,relief=tk.SUNKEN,anchor=tk.W,padx=10,bg='white')
+vel_display.place(x=670,y=30,width=100,height=30)
+
+# Lấy danh sách tất cả các cổng COM 
 com_ports = [port.device for port in serial.tools.list_ports.comports()]
 
 # Lấy danh sách Baud Rate
@@ -168,26 +250,36 @@ rate = [
     '57600', '76800', '115200', '230400', '460800', '576000', '921600']
 
 ''' Tạo các combobox cho các thông số liên quan tới UART'''
-# Tạo ô chọn cổng COM
+# Tạo ô chọn cổng COM cho điều khiển
 com_port = ttk.Combobox(root, values=com_ports, state='disabled')
 com_port.place(x=180, y=30, height=30, width=100)
 objects_0.append(com_port)
+objects_3.append(com_port)
+
+# Tạo ô chọn cổng COM cho GPS
+gps_port = ttk.Combobox(root, value = com_ports, state= 'disabled' )
+gps_port.place(x = 300, y= 30,height=30,width=100)
+objects_0.append(gps_port)
+objects_3.append(gps_port)
 
 # Tạo ô chọn Baud Rate
 rate_port = ttk.Combobox(root, values=rate, state='disabled')
-rate_port.place(x=300, y=30, height=30, width=100)
-rate_port.set(rate[4])
+rate_port.place(x=420, y=30, height=30, width=100)
+rate_port.set(rate[10])
 objects_0.append(rate_port)
+objects_3.append(rate_port)
 
 # Tạo ô chọn Data Bits
 data_port  = ttk.Combobox(root,values = ['7','8'],state='disabled')
 data_port.place(x=180, y=90, height=30, width=100)
 objects_0.append(data_port)
+objects_3.append(data_port)
 
 #Tạo ô chọn Stop Bit
 stop_port = ttk.Combobox(root,values=['1','1.5','2'],state='disabled')
 stop_port.place(x=300,y=90,height=30,width=100)
 objects_0.append(stop_port)
+objects_3.append(stop_port)
 
 ''' Tạo các nút '''
 # Tạo nút Open
@@ -201,7 +293,7 @@ objects_0.append(btn_close)
 
 # Tạo nút chọn cổng COM
 connect_button = tk.Button(root, text="Connect", state='disabled', command=connect_uart,bg='white')
-connect_button.place(x=415, y=30, height=30, width=70)
+connect_button.place(x=420, y=90, height=30, width=70)
 objects_0.append(connect_button)
 
 ''' chức năng auto'''
@@ -238,21 +330,21 @@ def forward(event):
             # Mở ảnh khi nút được nhấn
             forward_btn = PhotoImage(file='D:/STUDYING/Mechatronic Project/PROJECT-MECHATRONIC/GUI/2.png')
             #truyền UART cho bánh sau
-            if uart.is_open:
+            if b_uart.is_open:
                 start_bit = 'B'
                 back_for_adc = linear_scale.get()
                 direction = "T"
-                uart.write(bytearray([ord(start_bit),ord(direction),back_for_adc,ord(stop_bit)]))
+                b_uart.write(bytearray([ord(start_bit),ord(direction),back_for_adc,ord(stop_bit)]))
             else:
                 messagebox.showwarning('Warning','Please connect the UART')
         else:
             # Mở ảnh mặc định khi nút được thả ra
             forward_btn = PhotoImage(file='D:/STUDYING/Mechatronic Project/PROJECT-MECHATRONIC/GUI/1.png')
             # truyền UART cho bánh sau
-            if uart.is_open:
+            if b_uart.is_open:
                 start_bit = 'B'
                 back_for_adc = 0
-                uart.write(bytearray([ord(start_bit),back_for_adc,ord(stop_bit)]))
+                b_uart.write(bytearray([ord(start_bit),back_for_adc,ord(stop_bit)]))
             else:
                 messagebox.showwarning('Warning','Please connect the UART')
 
@@ -281,21 +373,21 @@ def reverse(event):
             # Mở ảnh khi nút được nhấn
             reverse_btn = PhotoImage(file='D:/STUDYING/Mechatronic Project/PROJECT-MECHATRONIC/GUI/4.png')
             #truyền UART giá trị tốc độ cho bánh sau
-            if uart.is_open:
+            if b_uart.is_open:
                 start_bit = 'B'
                 back_rev_adc = linear_scale.get()
                 direction = "L"
-                uart.write(bytearray([ord(start_bit),ord(direction),back_rev_adc,ord(stop_bit)]))
+                b_uart.write(bytearray([ord(start_bit),ord(direction),back_rev_adc,ord(stop_bit)]))
             else:
                 messagebox.showwarning('Warning','Please connect the UART')
         else:
             # Mở ảnh mặc định khi nút được thả ra
             reverse_btn = PhotoImage(file='D:/STUDYING/Mechatronic Project/PROJECT-MECHATRONIC/GUI/3.png')
             #truyền UART giá trị tốc độ cho bánh sau
-            if uart.is_open:
+            if b_uart.is_open:
                 start_bit = 'B'
                 back_rev_adc = 0
-                uart.write(bytearray([ord(start_bit),back_rev_adc,ord(stop_bit)]))
+                b_uart.write(bytearray([ord(start_bit),back_rev_adc,ord(stop_bit)]))
             else:
                 messagebox.showwarning('Warning','Please connect the UART')
 
@@ -330,8 +422,8 @@ def turn_slide(value):
     turn_adc = int(value)
     start_bit = 'F'
     uart_data = bytearray([ord(start_bit),abs(turn_adc),ord(stop_bit)])
-    if uart.is_open:
-        uart.write(uart_data)  
+    if f_uart.is_open:
+        f_uart.write(uart_data)  
 
 # Tạo ô trượt điều khiển trái phải
 turn_scale = tk.Scale(front_frame, from_=0, to=200, orient=tk.HORIZONTAL,bg='white',bd=1, 
@@ -356,18 +448,18 @@ def brake(event):
             # Mở ảnh khi nút được nhấn
             brake_btn = PhotoImage(file='D:/STUDYING\Mechatronic Project/PROJECT-MECHATRONIC/GUI/b_2.png')
             # truyền uart cho adc cho phanh
-            if uart.is_open:
+            if p_uart.is_open:
                 start_bit = 'P'
                 brake_adc = brake_slide.get()
-                uart.write(bytearray([ord(start_bit),brake_adc,ord(stop_bit)]))
+                p_uart.write(bytearray([ord(start_bit),brake_adc,ord(stop_bit)]))
         else:
             # Mở ảnh mặc định khi nút được thả ra
             brake_btn = PhotoImage(file='D:/STUDYING\Mechatronic Project/PROJECT-MECHATRONIC/GUI/b_1.png')
             # truyền uart cho adc cho phanh
-            if uart.is_open:
+            if p_uart.is_open:
                 start_bit = 'P'
                 brake_adc = 0
-                uart.write(bytearray([ord(start_bit),brake_adc,ord(stop_bit)]))
+                p_uart.write(bytearray([ord(start_bit),brake_adc,ord(stop_bit)]))
 
     btn_brake['image']= brake_btn
 
